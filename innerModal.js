195b584a0a65e9c8fac2ec8a41212bc391e2f920
Tashreeq Adams <tashreeqadams8@gmail.com>
2025-10-27 19:38:48 +0200

    Description of movie dynamically added to modal

---
+++ innerModal.js
@@ -0,0 +1,24 @@
+import { podcasts, genres, seasons } from "./data.js";
+
+const addDetails = () => {
+  const innerPodcast = document.getElementById("podcast-modal-content");
+
+  const podcastDetails = document.createElement("div");
+  podcastDetails.classList.add("inner-modal");
+
+  podcastDetails.innerHTML = `<img src="./assets/Placeholder_view_vector.svg.png" />
+            <div class="modal-info">
+              <p>Description</p>
+              <p id="modal-desc"></p>
+              <p>Genres</p>
+              <div id="modal-genres"></div>
+              <p id="modal-last-updated">Last Updated</p>
+            </div>
+            <div class="podcast-seasons">
+              <h1>Seasons</h1>
+            </div>`;
+
+  innerPodcast.appendChild(podcastDetails);
+};
+
+addDetails();
--- modal.js
+++ modal.js
@@ -7,13 +7,29 @@ const closeModal = document.getElementsByClassName("close");
 // Loop through all elements with class "thumbnail"
 for (let i = 0; i < openModal.length; i++) {
   openModal[i].addEventListener("click", () => {
-    modal.showModal(); // open modal
+    const currentPodcast = podcasts[i];
+    console.log("Current podcast:", currentPodcast);
+
+    if (currentPodcast && currentPodcast.id) {
+      const descEl = document.getElementById("modal-desc");
+
+      if (descEl) {
+        descEl.innerText = currentPodcast.description;
+      } else {
+        console.warn("Object ID not found");
+      }
+
+      modal.showModal();
+    } else {
+      console.warn("Podcast ID not found");
+    }
+    // console.log(currentPodcast.id);
   });
 }
 
 // Loop through all elements with class "close"
 for (let i = 0; i < closeModal.length; i++) {
   closeModal[i].addEventListener("click", () => {
-    modal.close(); // close modal
+    modal.close();
   });
 }
